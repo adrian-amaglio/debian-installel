1e44c06b0615703deb45afe61d7175be7129f75d
Adrian Amaglio <Vous@exemple.com>
2019-06-15 19:07:25 +0200

    more functionnalities

--- install.sh
+++ install.sh
@@ -38,16 +38,17 @@ release="stretch"
 
 usage[s]="Source repository of installed system"
 varia[s]=repo
-repo="http://ftp.fr.debian.org/debian"
+#repo=
+#repo="http://ftp.fr.debian.org/debian"
 repo="http://localhost:3142/ftp.fr.debian.org/debian"
 
 usage[n]="The hostname"
 varia[n]=hostname
 hostname="nohost"
 
-usage[p]="Root password. Will be read from stdin if empty."
-varia[p]=password
-password=
+#usage[p]="Root password. Will be read from stdin if empty."
+#varia[p]=password
+#password=
 
 usage[l]="System locale"
 varia[l]=locale
@@ -65,11 +66,8 @@ usage[v]="The installed OS starts in RAM"
 varia[v]=start_in_ram
 start_in_ram=false
 
-usage[d]="The device where we will install the system"
-varia[d]=root_device
-root_device=
 
-usage[b]="The device where we will install the boot"
+usage[b]="The device where grub will be installed"
 varia[b]=boot_device
 boot_device=
 
@@ -81,6 +79,10 @@ usage[o]="Only mount target"
 varia[o]=dry
 dry=false
 
+usage[O]="Secret output directory"
+varia[O]=secret_dir
+secret_dir="~/debian-secrets"
+
 . driglibash-args
 
 ###############################################################################
@@ -91,72 +93,26 @@ chroot_run(){
   run echo "$@" | chroot "$mnt"
 }
 
-mount_all(){
-  mount_partitions
-  mount_misc
-}
-
-mount_partitions(){
-  run mkdir -p "$mnt"
-  run mount "$root_device" "$mnt"
-  if [ -n "$boot_device" ] ; then
-    run mkdir -p "$mnt/boot"
-    run mount "$boot_device" "$mnt/boot"
-  fi
-  clean "umount $mnt -A --recursive"
-}
-
-mount_misc(){
-  run mount -t proc none "$mnt/proc"
-  # To access physical devices
-  run mount -o bind /dev "$mnt/dev"
-  run mount -o bind /sys "$mnt/sys"
-  # mount /dev/pts ? apt install complain about its absence
-}
-
 wait_for_user(){
   section "Time for a pause"
   run echo "Press 'Enter' to continue"
   read
 }
 
-try_test(){
-  if [ "$arg_test" != "false" ] ; then
-    section "Testing installed system"
-    run qemu-system-x86_64 "$bloc"
-  fi
-}
 
 root_or_die
 
-section "Choosing device"
-# The bloc device is where grub will be physically installed
-bloc=$(echo "$boot_device" | grep -o "^[a-zA-Z/]*")
-
-if "$dry" ; then
-  section "Dry Run - mount"
-  mount_all
-  wait_for_user
-  try_test
-  die 'End of dry script'
+section "Testing for existing secrets"
+secret_dir="$(realpath "$secret_dir/$hostname")"
+if [ -n "$(ls -A $secret_dir)" ]; then
+  die "Secret dir '$secret_dir' is not empty"
 fi
+run mkdir -p "$secret_dir"
 
-section "Reading password"
-if [ -z "$password" ] ; then
-  read password
-fi
-
-section "Formating"
-# TODO no confirmation ?
-run mkfs.ext4 "$root_device"
-run mkfs.ext2 "$boot_device"
-
-
-section "Mounting partitions"
-mount_partitions
 
 section "debootstraping"
-run debootstrap --arch "$arch" "$release" "$mnt" "$repo"
+run debootstrap --verbose --arch "$arch" "$release" "$mnt" "$repo"
+
 
 # boot crypted
 section "Installing cryptsetup in initramfs"
@@ -190,7 +146,7 @@ done
 
 
 section "Mounting additionnal items"
-mount_misc
+#mount_misc
 
 section "Configuring new system"
 uuid=$(blkid | grep "$root_device" | cut -d ' ' -f 2)
@@ -217,11 +173,11 @@ if [ -n "$network" ] ; then
   # Disable the unpredictable naming (since we are not on the future host)
   run ln -s /dev/null "$mnt/etc/udev/rules.d/80-net-setup-link.rules"
 
-  interface="$(echo "$static_network" | cut -d '-' -f 1)"
-  ip="$(echo "$static_network" | cut -d '-' -f 2)"
+  interface="$(echo "$network" | cut -d '-' -f 1)"
+  ip="$(echo "$network" | cut -d '-' -f 2)"
   netmask="$(ipcalc $ip -n |grep Netmask| cut -d ' ' -f 4)"
-  ip="$(echo "$static_network" | cut -d '/' -f 1)"
-  gateway="$(echo "$static_network" | cut -d '-' -f 3)"
+  ip="$(echo "$network" | cut -d '/' -f 1)"
+  gateway="$(echo "$network" | cut -d '-' -f 3)"
 
   if [ "$ip" = "dhcp" ] ; then
     run cat >> "$mnt/etc/network/interfaces" <<EOF
@@ -241,18 +197,7 @@ EOF
 fi
 
 
-section "Creating root SSH key to connect"
-ssh_key_passphrase=="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 200)"
-run ssh-keygen -b 4096 -f ../ssh/$hostname -P "$ssh_key_passphrase"
-run mkdir -p "$mnt/root/.ssh/"
-cat ../ssh/$hostname.pub >> "$mnt/root/.ssh/authorized_keys"
-run sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/g' "$mnt/etc/ssh/sshd_config"
-cat > ../ssh/config <<EOF
-Host $hostname
-  HostName $ip
-  User root
-  IdentityFile "$(pwd)/../ssh/$hostname"
-EOF
+# TODO add dyndn service
 
 
 if "$start_in_ram" ; then
@@ -261,28 +206,27 @@ if "$start_in_ram" ; then
 fi
 
 
-
 section "Installing selected software"
+#XXX use chroot_run
 chroot "$mnt" <<EOF
   export DEBIAN_FRONTEND=noninteractive
   apt-get update  -q -y 
-  apt-get install -q -y linux-image-amd64 console-data grub2 locales $install
+  apt-get install -q -y linux-image-amd64 console-data grub2 locales sudo lvm2 $install
 EOF
+# TODO watershed ?
+
 
 section "Installing custom packs"
 for pack in "$packs" ; do
   if [ -n "$pack" ] ; then
   case "$pack" in
     *sysadmin*)
-      chroot_run 'apt install vim openssh-server git'
+      chroot_run 'DEBIAN_FRONTEND=noninteractive apt install vim openssh-server git'
       chroot_run 'git clone https://github.com/adrianamaglio/driglibash && cd driglibash && cp driglibash-* /usr/bin && cd .. && rm -rf driglibash'
     ;;
     *webserver*)
       echo 'Nginx will be installed, just add your webapp conf in /etc/nginx/sites-enabled'
-      chroot_run 'apt install nginx ; systemctl enable nginx ; systemctl start nginx'
-    ;;
-    *network*)
-      chroot_run 'git clone https://github.com/dahus/gateway && cd gateway && gateway.sh -i && cd .. && rm -rf gateway'
+      chroot_run 'DEBIAN_FRONTEND=noninteractive apt install nginx ; systemctl enable nginx ; systemctl start nginx'
     ;;
     *)
       die "pack '$pack' not supported"
@@ -292,11 +236,28 @@ for pack in "$packs" ; do
   fi
 done
 
+
+section "Creating root SSH key to connect"
+ssh_key_passphrase=="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 200)"
+run ssh-keygen -b 4096 -f "$secret_dir/$hostname" -P "$ssh_key_passphrase"
+run mkdir -p "$mnt/root/.ssh/"
+run mkdir -p "$secret_dir"
+cat "$secret_dir/$hostname.pub" >> "$mnt/root/.ssh/authorized_keys"
+run sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/g' "$mnt/etc/ssh/sshd_config"
+cat > "$secret_dir/ssh_conf" <<EOF
+Host $hostname
+  HostName $ip
+  User root
+  IdentityFile "$secret_dir/$hostname"
+EOF
+
+
 section "Generating locales"
 chroot "$mnt" <<EOF
   echo "$locale" > "/etc/locale.gen"
   locale-gen
 EOF
+
 
 section "Installing grub"
 chroot "$mnt" <<EOF
@@ -304,21 +265,14 @@ chroot "$mnt" <<EOF
   grub-install "$bloc"
 EOF
 
+
 section "Executing custom commands"
 for cmd in "${execute[@]}" ; do
   chroot_run "$cmd"
 done
 
-section "Setting root password"
-if [ -n "$password" ] ; then
-  chroot_run "echo -e \"$password\n$password\" | passwd"
+
+if [ "$arg_test" != "false" ] ; then
+  section "Testing installed system"
+  run qemu-system-x86_64 "$bloc"
 fi
-
-if "$keep_and_wait" ; then
-  wait_for_user
-fi
-
-section "Cleaning fs"
-clean
-
-try_test
